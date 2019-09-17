f24b5645991735e2a0d39b3dc956d92358f094b1
UNLegume <44322273+UNLegume@users.noreply.github.com>
2019-09-17 17:51:26 +0900

    list

--- ReactMap/src/screens/FriendList/FriendListScreen.js
+++ ReactMap/src/screens/FriendList/FriendListScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, ScrollView} from 'react-native';
+import { View, Text, ScrollView, SafeAreaView} from 'react-native';
 import {Ionicons} from '@expo/vector-icons';
 import { Button, Input, Card, SearchBar, ListItem, Divider} from 'react-native-elements';
 import s from './styles';
@@ -9,7 +9,7 @@ class FriendListScreenView extends React.Component{
     render(){
         var friendlist = [];
 
-        for(let i = 0; i<30; i++){
+        for(let i = 0; i<20; i++){
             friendlist.push(
                 <ListItem
                 key={i}
@@ -18,7 +18,7 @@ class FriendListScreenView extends React.Component{
                     containerStyle={{
                         backgroundColor:'#FFF',
                     }}
-                bottomDivider='true'
+                bottomDivider
                 />
             );
         }
@@ -53,16 +53,17 @@ class FriendListScreenView extends React.Component{
                 marginTop:10,
                 borderTopLeftRadius:20,
                 borderTopRightRadius:20,
+                flex:1,
                 }}>
                 <View style={{marginVertical:5,marginLeft:30,}}>
                     <Text style={{fontSize:24,color:'#fff'}}>Koyanagi</Text>
                     <Text style={{fontSize:18,color:'#fff',marginTop:5,}}>Recommend</Text>
                 </View>
-                <View style={{ marginTop:-1 }}>
-                    <ScrollView style={{
-
-                    }}>
+                <View style={{ marginTop:-1,flex:1 }}>
+                    <ScrollView>
+                        <SafeAreaView style={{flex:1}}>
                         { friendlist }
+                        </SafeAreaView>
                     </ScrollView>
                 </View>
             </View>
